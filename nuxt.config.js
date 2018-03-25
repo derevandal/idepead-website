ef9b3ff76bb8a33c0e3fe867e18a22cc6596db87
André Van Dal <andre@lenyeg.com.br>
2018-03-25 12:55:44 -0300

    add eslint with vue on save and with run script

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,50 +2,64 @@ module.exports = {
   /*
   ** Build configuration
   */
-  build: {},
+  build: {
+    extend(config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
   /*
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
-  head: {
-  },
+  head: {},
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#2A4E52' },
+  loading: { color: "#2A4E52" },
   /*
   ** Customize app manifest
   */
   manifest: {
-    name: 'Fale conosco - IDEPead',
-    short_name: 'IDEPead',
-    lang: 'pt-br',
-    theme_color: '#6BC7D2'
+    name: "Fale conosco - IDEPead",
+    short_name: "IDEPead",
+    lang: "pt-br",
+    theme_color: "#6BC7D2"
   },
 
   meta: {
-    name: 'Fale conosco - IDEPead',
-    description: 'Fale conosco a respeito de algum serviço prestado por nossos polos educaionais.',
-    lang: 'pt-br',
-    ogHost: 'https://idepead.com.br/',
-    ogUrl: 'https://idepead.com.br/',
+    name: "Fale conosco - IDEPead",
+    description:
+      "Fale conosco a respeito de algum serviço prestado por nossos polos educaionais.",
+    lang: "pt-br",
+    ogHost: "https://idepead.com.br/",
+    ogUrl: "https://idepead.com.br/",
     nativeUI: true
   },
   /*
   ** Modules
   */
   modules: [
-    '@nuxtjs/pwa',
-    [
-      'nuxt-bulma-slim',
+    ["@nuxtjs/pwa",
       {
-        variablesPath: 'assets/scss/variables.scss',
-        additionalPaths: ['assets/scss/cosmo.scss', 'assets/scss/fonts.scss']
+        workbox: false
+      }
+    ],
+    [
+      "nuxt-bulma-slim",
+      {
+        variablesPath: "assets/scss/variables.scss",
+        additionalPaths: ["assets/scss/cosmo.scss", "assets/scss/fonts.scss"],
+        whitelist: ['.control.loading']
       }
     ]
   ],
 
-  plugins: [
-    '~plugins/v-validate'
-  ]
+  plugins: ["~plugins/v-validate"]
 }
